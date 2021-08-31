9cf28c8f9b3f0a69fdd373bfbcc4d14cf771659e
“jelin.mi” <jelin.mi@gmail.com>
2021-08-31 21:30:14 +0200

    adding semicolons

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-let count = 5
+let count = 5;
 
 function render(){
-    let container = document.getElementById('pokemon')
-    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`
+    let container = document.getElementById('pokemon');
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
 }
 
 
-let prev = document.getElementById('prev')
-let next = document.getElementById('next')
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
 
 prev.onclick = function(){
     if (count > 1) {
-        count = count - 1
-        render()
+        count = count - 1;
+        render();
     }
    
-}
+};
 
 next.onclick = function(){
     if (count < 650){
-        count = count + 1
-        render()
+        count = count + 1;
+        render();
     }
     
-}
+};
 
-render()
+render();
 
